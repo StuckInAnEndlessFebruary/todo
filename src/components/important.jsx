0c0c9a7f30117f7222ebe86872fb6295c3817863
Mina <stuckinanendlessfebruary@gmail.com>
2024-08-14 20:09:58 +0330

    tried using router

---
+++ src/components/important.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+const Important = () => {
+  return <h1>important</h1>;
+};
+
+export default Important;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import Sidebar from "./components/sidebar";
+import Home from "./components/home";
+import MyDay from "./components/myDay";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "tailwindcss/tailwind.css";
+import Important from "./components/important";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Sidebar />}>
+          <Route index element={<Home />} />
+          <Route path="/myDay" element={<MyDay></MyDay>} />
+          <Route path="/important" element={<Important></Important>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
